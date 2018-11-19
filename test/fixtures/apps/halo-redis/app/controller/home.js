1d930ea23da63328ba1152b92fb0604fd80d27a7
yjk <t1m2o3e4@gmail.com>
2018-11-19 16:39:45 +0800

    use custom egg-socket.io to support sentinel mode

--- test/fixtures/apps/halo-redis/app/controller/home.js
+++ test/fixtures/apps/halo-redis/app/controller/home.js
@@ -3,12 +3,8 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    const data = await this.service.test.get(123);
-    this.ctx.body = data.name;
-  }
 
-  async redis() {
+  async index() {
     const { ctx, app } = this;
     await app.redis.set('foo', 'bar');
     ctx.body = await app.redis.get('foo');
